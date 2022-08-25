79510e06ece8e8397713b4b5d9c10d905afe77c3
Jeff Andrews <babajeff8170@gmail.com>
2022-08-25 17:06:40 -0400

    preparing logic for fetch of single cocktail record

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchSingleCocktail } from "../redux/features/cocktailSlice";
+import { Link, useParams } from "react-router-dom";//if the route uses a parameter, you need useParams hook
 
 const SingleCocktail = () => {
+  const { cocktail, loading } = useSelector();//access the store/state
+  const dispatch = useDispatch();
+
+  
   return (
     <div>
       <h2>SingleCocktail</h2>
--- src/redux/features/cocktailSlice.js
+++ src/redux/features/cocktailSlice.js
@@ -14,11 +14,20 @@ export const fetchCocktails = createAsyncThunk(
   }
 );
 
+export const fetchSingleCocktail = createAsyncThunk(
+  "cocktails/fetchSingleCocktail",
+  async ({id}) => {
+    return fetch(
+      `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    ).then((res) => res.json());
+  }
+);
+
 const cocktailSlice = createSlice({
   name: "cocktails",
   initialState: {
     cocktails: [],
-    cocktail: [],
+    cocktail: [],//single cocktail goes here!
     loading: false,
     error: null,
   },
@@ -31,6 +40,17 @@ const cocktailSlice = createSlice({
       state.cocktails = action.payload.drinks;
     },
     [fetchCocktails.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [fetchSingleCocktail.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchSingleCocktail.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.cocktail = action.payload.drinks;
+    },
+    [fetchSingleCocktail.rejected]: (state, action) => {
       state.loading = false;
       state.error = action.payload;
     },
